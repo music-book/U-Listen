2abedd8ca08d090ecafdd8666b0c0d12f0ab4782
jun <qiaojun@followme.cn>
2020-04-06 22:26:24 +0800

    feat: 登录优化

--- src/pages/song-list/index.vue
+++ src/pages/song-list/index.vue
@@ -65,12 +65,6 @@
             </el-form-item>
           </el-form>
         </div>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogVisible = false"
-            >确 定</el-button
-          >
-        </span>
       </el-dialog>
     </div>
     <div class="song-list-content" v-loading="loading">
